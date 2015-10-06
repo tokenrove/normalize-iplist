d0e20766bebd7833af4e026a6a91c66b6d428a4b
Julian Squires <julian@mobcap>
2015-10-06 15:29:42 -0400

    Use more lenient compiler flags
    To support older versions of GCC.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -3,6 +3,6 @@ require 'mkmf'
 extension_name = 'normalize_iplist'
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
-$CFLAGS = %w(-Wall -Wextra -pedantic -std=gnu11 -O3 -g).join(' ')
+$CFLAGS = %w(-Wall -Wextra -std=gnu99 -fms-extensions -O3 -g).join(' ')
 
 create_makefile(extension_name)
